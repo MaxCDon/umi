5315906673ed60c5c4d66444e1eb3aaf123650e9
禾承 <chengdonghang.cdh@alibaba-inc.com>
2023-03-07 19:43:39 +0800

    fix(iife): test improve esbuild iife (#10675)

--- scripts/changelog.ts
+++ scripts/changelog.ts
@@ -3,7 +3,7 @@ import { getReleaseNotes } from './utils/getReleaseNotes';
 
 (async () => {
   const { releaseNotes } = await getReleaseNotes('4.0.54');
-  console.log(6666);
+  console.log(7777777);
   console.log(releaseNotes);
 })().catch((e) => {
   console.error(e);
